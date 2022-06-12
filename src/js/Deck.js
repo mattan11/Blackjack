541145e3148507a5cc646dc8dad10fd3db410675
Marek Witkowski <marek.witkowski@onet.com.pl>
2022-06-12 19:14:02 +0200

    display cards for player and AI

--- src/js/Deck.js
+++ src/js/Deck.js
@@ -19,4 +19,8 @@ export class Deck {
 
     return this.cards;
   }
+
+  pickOne() {
+    return this.cards.pop();
+  }
 }
--- src/js/Game.js
+++ src/js/Game.js
@@ -1,7 +1,37 @@
 import {Deck} from './Deck';
 import {Player} from './Player';
 
-const player = new Player('Marek');
+class Game {
+  constructor({playersCards, dealersCards, player}) {
+    this.player = player;
+    this.dealer = new Player('Krupier');
+    this.playersCards = playersCards;
+    this.dealersCards = dealersCards;
+    this.deck = new Deck();
+    this.deck.shuffle();
+  }
 
-const deck = new Deck();
-deck.shuffle();
+  run() {
+    this.dealCards();
+  }
+
+  dealCards() {
+    for (let i = 0; i < 2; i++) {
+      const cardForPlayer = this.deck.pickOne();
+      this.player.hand.addCard(cardForPlayer);
+      this.playersCards.appendChild(cardForPlayer.render());
+
+      const cardForAI = this.deck.pickOne();
+      this.player.hand.addCard(cardForAI);
+      this.dealersCards.appendChild(cardForAI.render());
+    }
+  }
+}
+
+const player = new Player('Marek');
+const game = new Game({
+  player,
+  playersCards: document.getElementById('playersCards'),
+  dealersCards: document.getElementById('dealersCards'),
+});
+game.run();
