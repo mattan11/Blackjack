6a3d2722ed8e68e19733df0c6887ffa00d63e1cf
Marek Witkowski <marek.witkowski@onet.com.pl>
2022-06-12 19:41:57 +0200

    handle hitting cards for player

--- src/js/Game.js
+++ src/js/Game.js
@@ -3,7 +3,9 @@ import {Player} from './Player';
 import {Table} from './Table';
 
 class Game {
-  constructor({player, table}) {
+  constructor({player, table, hitButton, standButton}) {
+    this.hitButton = hitButton;
+    this.standButton = standButton;
     this.player = player;
     this.dealer = new Player('Krupier');
     this.table = table;
@@ -12,7 +14,14 @@ class Game {
   }
 
   run() {
+    this.hitButton.addEventListener('click', (event) => this.hitCard());
     this.dealCards();
+  }
+
+  hitCard() {
+    const card = this.deck.pickOne();
+    this.player.hand.addCard(card);
+    this.table.showPlayersCard(card);
   }
 
   dealCards() {
@@ -34,6 +43,8 @@ const table = new Table(
 );
 const player = new Player('Marek');
 const game = new Game({
+  hitButton: document.getElementById('hit'),
+  standButton: document.getElementById('stand'),
   player,
   table,
 });
